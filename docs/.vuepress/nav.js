a7c63d5de621068f9cfdea9c5cf0385e508df311
liwei <15010199100@163.com>
2020-01-17 13:37:30 +0800

    .

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -1,67 +1,65 @@
 // 标题栏
 module.exports = [
   {
-    text: 'web前端',
+    text: 'WEB前端',
     items: [
-      // { text: 'CSS', link: '/web/css/' },
-      // { text: 'HTML', link: '/web/html/' },
-      { text: 'JavaScript', link: '/web/js/' },
-    ]
-  },
-  // {
-  //   text: '微信端',
-  //   items: [
-  //     { text: '公众号', link: '/vue/' },
-  //     { text: '小程序', link: '/vue/' },
-  //   ]
-  // },
-  // {
-  //   text: '前端框架',
-  //   items: [
-  //     { text: 'VUE', link: '/vue/' },
-  //     { text: 'React', link: '/vue/' },
-  //   ]
-  // },
-  // {
-  //   text: '服务器开发',
-  //   items: [
-  //     { text: 'Node.js', link: '/vue/' },
-  //   ]
-  // },
-  {
-    text: '开发工具',
-    items: [
-      { text: 'vscode', link: 'https://code.visualstudio.com/' },
-      { text: '微信开发者工具', link: 'https://developers.weixin.qq.com/miniprogram/dev/devtools/download.html' },
+      {
+        text: '前端页面',
+        items: [
+          { text: 'CSS', link: '/web/css/' },
+          { text: 'HTML', link: '/web/html/' },
+          { text: 'JavaScript', link: '/web/js/' },
+        ]
+      },
+      {
+        text: '前端框架',
+        items: [
+          { text: 'Vue.js', link: '' },
+          { text: 'React', link: '' },
+        ]
+      },
+      {
+        text: '数据可视化',
+        items: [
+          { text: 'echarts', link: ''},
+        ]
+      },
     ]
   },
   {
-    text: '组件库',
+    text: '微信端',
     items: [
-      {
-        text: '移动端',
-        items: [
-          { text: 'Cube-UI', link: 'https://didi.github.io/cube-ui/#/zh-CN' },
-          { text: 'Vant', link: 'https://youzan.github.io/vant/#/zh-CN/' },
-        ]
-      },
-      {
-        text: 'PC端',
-        items: [
-          { text: 'Element-UI', link: 'https://element.eleme.cn/#/zh-CN' },
-        ]
-      },
-      {
-        text: '小程序',
-        items: [
-          { text: 'Vant-Weapp', link: 'https://youzan.github.io/vant-weapp/#/intro' },
-        ]
-      },
+      { text: '公众号', link: ''},
+      { text: '小程序', link: ''},
+    ]
+  },
+  {
+    text: '原生APP开发',
+    items: [
+      { text: 'React Native', link: ''},
+      { text: 'Flutter', link: ''},
+    ]
+  },
+  {
+    text: '服务器开发',
+    items: [
+      { text: 'Node.js', link: ''},
+      { text: 'Mysql', link: ''},
     ]
   },
   {
     text: '站外链接',
     items: [
+      { text: 'Echarts官网', link: 'https://www.echartsjs.com/zh/index.html' },
+      {
+        text: 'UI组件库',
+        items: [
+          { text: 'Cube-UI', link: 'https://didi.github.io/cube-ui/#/zh-CN' },
+          { text: 'Vant', link: 'https://youzan.github.io/vant/#/zh-CN/' },
+          { text: 'Element-UI', link: 'https://element.eleme.cn/#/zh-CN' },
+          { text: 'Vant-Weapp', link: 'https://youzan.github.io/vant-weapp/#/intro' },
+        ]
+      },
       {
         text: '在线服务',
         items: [
@@ -78,42 +76,4 @@ module.exports = [
       }
     ]
   }
-
-
-
-  // {
-  //   text: '懵逼指南', link: '/guide/'
-  // },
-  // {
-  //   text: '面试宝典',
-  //   items: [
-  //     { text: '初级开发篇', link: '/baodian/zero/' },
-  //     { text: '中高进阶篇', link: '/baodian/high/' },
-  //   ]
-  // },
-  // {
-  //   text: '工具箱',
-  //   items: [
-  //     {
-  //       text: '在线编辑',
-  //       items: [
-  //         { text: '图片压缩', link: 'https://tinypng.com/' }
-  //       ]
-  //     },
-  //     {
-  //       text: '在线服务',
-  //       items: [
-  //         { text: '阿里云', link: 'https://www.aliyun.com/' },
-  //         { text: '腾讯云', link: 'https://cloud.tencent.com/' }
-  //       ]
-  //     },
-  //     {
-  //       text: '博客指南',
-  //       items: [
-  //         { text: '掘金', link: 'https://juejin.im/' },
-  //         { text: 'CSDN', link: 'https://blog.csdn.net/' }
-  //       ]
-  //     }
-  //   ]
-  // }
 ]
